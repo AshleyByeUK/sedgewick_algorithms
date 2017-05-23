b494eb5ad1b4e14264f8351fdd84ee9c5f1e5684
ash <ashley+git@ashleybye.uk>
2017-05-23 20:19:08 +0100

    Add comments

--- src/main/java/uk/ashleybye/sedgewick/graph/DepthFirstPaths.java
+++ src/main/java/uk/ashleybye/sedgewick/graph/DepthFirstPaths.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 import uk.ashleybye.sedgewick.collections.Stack;
 
 /**
- * Uses Depth First Search to visit all getNumVertices connected to a given source vertex. The end vertex
- * of each edge (v, w) traversed for the first time is recorded so that the path from the source
+ * Uses Depth First Search to visit all vertices connected to a given source vertex. The end vertex
+ * of each edge (u, v) traversed for the first time is recorded so that the path from the source
  * vertex to any other given vertex can be determined.
  */
 public class DepthFirstPaths {
@@ -14,6 +14,13 @@ public class DepthFirstPaths {
   private int[] edgeTo;
   private final int sourceVertex;
 
+  /**
+   * Construct an instance of DepthFirstPaths to compute the paths from the given source vertex in
+   * the provided graph.
+   *
+   * @param graph The graph to search for paths.
+   * @param sourceVertex The vertex to search for paths from.
+   */
   public DepthFirstPaths(Graph graph, int sourceVertex) {
     markedVertices = new boolean[graph.getNumVertices()];
     edgeTo = new int[graph.getNumVertices()];
@@ -21,6 +28,13 @@ public class DepthFirstPaths {
     depthFirstSearch(graph, sourceVertex);
   }
 
+  /**
+   * Searches the given graph to find paths from the given source vertex. The end vertex of each (u,
+   * v) edge is recorded in {@code edgeTo}.
+   *
+   * @param graph The graph to search for paths.
+   * @param vertex The vertex to search for paths from.
+   */
   private void depthFirstSearch(Graph graph, int vertex) {
     markedVertices[vertex] = true;
     for (int adjacentVertex : graph.adjacentTo(vertex)) {
@@ -31,10 +45,24 @@ public class DepthFirstPaths {
     }
   }
 
+  /**
+   * Returns true if there is a path to the specified vertex; false, otherwise.
+   *
+   * @param vertex The vertex to query.
+   *
+   * @return True if there is a path to the given vertex; false, otherwise.
+   */
   private boolean hasPathTo(int vertex) {
     return markedVertices[vertex];
   }
 
+  /**
+   * Returns the path from this instances source vertex to the specified vertex.
+   *
+   * @param vertex The vertex to retrieve the path to.
+   *
+   * @return The paths from the source vertex.
+   */
   public Optional<Iterable<Integer>> pathTo(int vertex) {
     if (!hasPathTo(vertex)) {
       return Optional.empty();
